dffae6038ed3b4fd6391a9d31efcde696e38901d
Enzo L. <fenzolemos@gmail.com>
2024-08-22 16:56:57 -0300

    feat AdicionaAtividade7

---
+++ .java/Att7.java
@@ -0,0 +1,14 @@
+package pack1;
+import java.util.*;
+public class Att7 {
+	public static void main (String [] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Insira um número: ");
+		double val = sc.nextDouble();
+		if (val > 10) {
+			System.out.println("É MAIOR QUE 10!");
+		} else {
+			System.out.println("NÃO É MAIOR QUE 10!");
+		}
+	}
+}
